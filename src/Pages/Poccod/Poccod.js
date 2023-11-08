7054ff4162d7ed7ca51adce66b07fe2fc424bd4a
tabrid <mollik15-5096@diu.edu.bd>
2023-11-08 20:22:15 +0600

    procchod

--- src/Pages/Poccod/Poccod.js
+++ src/Pages/Poccod/Poccod.js
@@ -5,17 +5,17 @@ const Poccod = () => {
         <div className="card lg:card-side bg-base-100 shadow-xl m-10">
         <figure className="w-1/4 h-1/2 rounded-full">
           <img
-            src="https://i.ibb.co/hFKnrDC/24-23-08.jpg"
+            src="https://i.ibb.co/vdvmwDW/IMG-6813.jpg"
             alt="Album"
           />
         </figure>
         <div className="card-body flex-col justify-center items-center">
-          <h2 className="card-title">My Name Is Riyad Ali Mollik</h2>
-          <p>Id : 221-15-5096</p>
+          <h2 className="card-title">My Name Is MD Abrar Awal Procchod</h2>
+          <p>Id : 221-15-5912</p>
           <h2 className="text-2xl">Assignment</h2>
-          <Link to="https://drive.google.com/drive/folders/14j6ueHtQrES2BZ7n-9qz95wr6RjCxaj7?usp=sharing"><p className="text-cyan-400 underline-offset-1">SRS V1</p></Link>
-          <Link to=""><p className="text-cyan-400 underline-offset-1">SRS V2</p></Link>
-          <Link to=""><p className="text-cyan-400 underline-offset-1">SRS V3</p></Link>
+          <Link to="https://drive.google.com/drive/folders/1DDgC4Tkbep_fm_m1EgxikCUHVRVAW_eT?usp=drive_link"><p className="text-cyan-400 underline-offset-1">SRS V1</p></Link>
+          <Link to="https://drive.google.com/drive/folders/1ATKnc9CFxfDx1no5lXHD8ZYbuFYnsicu?usp=drive_link"><p className="text-cyan-400 underline-offset-1">SRS V2</p></Link>
+          <Link to="https://drive.google.com/drive/folders/16iBXggo4Bve1oz-3v0z-iJoGYf9Kl7-Q?usp=drive_link"><p className="text-cyan-400 underline-offset-1">SRS V3</p></Link>
         </div>
       </div>
     );
